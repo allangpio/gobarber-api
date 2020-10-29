bac76afab4d62c0fe395e3e1d35ab4c389699a5a
allangpio <gpioallan@gmail.com>
2020-10-29 08:58:15 -0300

    importing fakehash provider

--- src/modules/users/services/CreateUsersService.spec.ts
+++ src/modules/users/services/CreateUsersService.spec.ts
@@ -1,12 +1,16 @@
 import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fake/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import CreateUserService from './CreateUserService';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
     const createUser = new CreateUserService(
-      fakeUsersRepository
+      fakeUsersRepository,
+      fakeHashProvider,
     );
 
     const user = await createUser.execute({
@@ -20,8 +24,11 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with an already registered email', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
     const createUser = new CreateUserService(
-      fakeUsersRepository
+      fakeUsersRepository,
+      fakeHashProvider,
     );
 
     await createUser.execute({
